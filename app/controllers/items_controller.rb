85cb2f106d28f3212d5cee5fa74a414b072ac70c
SheeTabz <wmuriithitabitha@gmail.com>
2022-11-23 19:03:17 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+   
+    return render json: items, include: :user
   end
 
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+user = User.find(params[:user_id])
+item = user.items.create(items_params)
+render json: item, status: :created
+  end
+
+  private
+  def items_params
+    params.permit(:name, :description, :price)
+  end
+
+  def user_not_found
+    render json: {error: 'Could not find User with #{params[:user_id]}'}, status: :not_found
+  end
 end
